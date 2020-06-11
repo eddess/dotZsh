657ef078fa4ddbd230b73392d56b49b287d97ee7
Eddy Essien <eddy.essien@gmail.com>
2020-06-10 21:22:01 -0700

    Make shell sh compatible

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 for zf in "zshenv" "zprofile" "zshrc" "zprofile" "zlogin" "zlogout" 
 do
-	[[ -f "${HOME}/.zsh/${zf}" ]] && ln -s ${HOME}/.zsh/${zf} ${HOME}/.${zf}
+	[ -f "${HOME}/.zsh/${zf}" ] && ln -s ${HOME}/.zsh/${zf} ${HOME}/.${zf}
 done
